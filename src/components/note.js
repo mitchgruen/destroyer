a524f3a6dd644fa53ade770c7c5223c1d7dd2a13
mitchgruen <103967990+mitchgruen@users.noreply.github.com>
2024-01-22 16:22:44 -0500

    Remove unused variables

--- src/components/note.js
+++ src/components/note.js
@@ -1,12 +1,9 @@
 import { Rnd } from 'react-rnd';
 import { useState, useEffect, useRef } from 'react';
-import NoteStyle from '../styles/NoteStyle.css';
 import { useSelector, useDispatch } from 'react-redux';
 import {
-  addNote,
   deleteNote,
   setContent,
-  setZIndex,
   setSize,
   setPosition,
   setMinimized,
@@ -94,14 +91,14 @@ function Note({ uuid, zIndexCounter, setZIndexCounter, minuteTick }) {
     }
   }
 
-  function getTimeDifference(date1, date2) {
-    const difference = date2 - date1;
+  // function getTimeDifference(date1, date2) {
+  //   const difference = date2 - date1;
 
-    const hours = Math.floor(difference / (1000 * 60 * 60));
-    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  //   const hours = Math.floor(difference / (1000 * 60 * 60));
+  //   const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
 
-    return { hours, minutes };
-  }
+  //   return { hours, minutes };
+  // }
 
   // go through this to make sure you understand how it works
   function get24HourCountdown(createdAt) {
