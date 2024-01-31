bbe833e087bed556e53b7b089c0a2f88b2885eef
mitchgruen <103967990+mitchgruen@users.noreply.github.com>
2024-01-31 16:47:51 -0500

    Redirect * to login

--- src/App.js
+++ src/App.js
@@ -1,62 +1,81 @@
 import './styles/AppStyle.css';
 import Note from './components/note';
 import Settings from './components/settings';
-import Trash from './components/trash'
-import { useState, useEffect } from "react";
+import Trash from './components/trash';
+import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { addNote } from './app/slices/noteSlice';
-
+import axios from 'axios';
 
 function App() {
-  const [zIndexCounter, setZIndexCounter] = useState(0)
-  const [minuteTick, setMinuteTick] = useState(0)
+  const [zIndexCounter, setZIndexCounter] = useState(0);
+  const [minuteTick, setMinuteTick] = useState(0);
   const [isSettingsOpen, setSettingsOpen] = useState(false);
   const [isTrashOpen, setTrashOpen] = useState(false);
 
   // const notes = useSelector(state => state.notes)
-  
+
   // next I need to grab the coordinates of a click event whenever one takes place
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setMinuteTick(n => n + 1)
-    }, 60 * 1000)
-    return () => clearInterval(interval)
-  }, [])
+      setMinuteTick((n) => n + 1);
+    }, 60 * 1000);
+    return () => clearInterval(interval);
+  }, []);
 
-  const noteState = useSelector(state => state.notes)
-  const dispatch = useDispatch()
+  const noteState = useSelector((state) => state.notes);
+  const dispatch = useDispatch();
 
   function handleClick() {
-    setZIndexCounter(zIndexCounter + 1)
-    dispatch(addNote({z: zIndexCounter}))
+    setZIndexCounter(zIndexCounter + 1);
+    dispatch(addNote({ z: zIndexCounter }));
   }
-  
+
+  // API Test
+  useEffect(() => {
+    axios.get('http://54.85.203.34:8000/')
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err))
+  }, [])
+
+  useEffect(() => {
+    axios.get('http://54.85.203.34:8000/test')
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err))
+  }, [])
+
   return (
-    <div className='app'>
-      <div className={`main ${isSettingsOpen || isTrashOpen ? 'blur-background' : ''}`}>
-        <div className='navbar'>
-          <button onClick={handleClick} className='navbar-icon'>
+    <div className="app">
+      <div
+        className={`main ${
+          isSettingsOpen || isTrashOpen ? 'blur-background' : ''
+        }`}
+      >
+        <div className="navbar">
+          <div>Destroyer</div>
+          <button onClick={handleClick} className="navbar-icon">
             <i className="fa-solid fa-plus"></i>
           </button>
-          <div>
-            JotSpace
-          </div>
-          <button onClick={() => setSettingsOpen(true)} className='navbar-icon'>
+          <button onClick={() => setSettingsOpen(true)} className="navbar-icon">
             <i className="fa-solid fa-gear"></i>
           </button>
-          <button onClick={() => setTrashOpen(true)} className='navbar-icon'>
-            <i class="fa-solid fa-trash"></i>
+          <button onClick={() => setTrashOpen(true)} className="navbar-icon">
+            <i className="fa-solid fa-trash"></i>
           </button>
         </div>
-        {
-          Object.keys(noteState).map((uuid, index) => (
-            <Note key={uuid} uuid={uuid} zIndexCounter={zIndexCounter} setZIndexCounter={setZIndexCounter} minuteTick={minuteTick}/>
-          ))
-        }
+        {Object.keys(noteState).map((uuid, index) => (
+          <Note
+            key={uuid}
+            uuid={uuid}
+            zIndexCounter={zIndexCounter}
+            setZIndexCounter={setZIndexCounter}
+            minuteTick={minuteTick}
+          />
+        ))}
       </div>
       <Settings onClose={() => setSettingsOpen(false)} isSettingsOpen={isSettingsOpen}/>
-      <Trash onClose={() => setTrashOpen(false)} isTrashOpen={isTrashOpen}/>
+      <Trash onClose={() => setTrashOpen(false)} isTrashOpen={isTrashOpen} />
     </div>
   );
 }
