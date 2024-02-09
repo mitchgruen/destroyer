b60481946c428cd365f4b29591ec244cd9a86479
mitchgruen <103967990+mitchgruen@users.noreply.github.com>
2024-02-09 14:17:53 -0500

    Add new logo svg

--- src/components/router.js
+++ src/components/router.js
@@ -2,6 +2,7 @@ import App from '../App';
 import Login from './login';
 import Register from './register';
 import ForgotPassword from './forgotpassword';
+import Welcome from './welcome';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 export default function Router() {
@@ -10,6 +11,7 @@ export default function Router() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/welcome" element={<Welcome />} />
         <Route path="/forgotpassword" element={<ForgotPassword />} />
         <Route path="/app" element={<App />} />
         <Route path="*" element={<Navigate replace to="/login" />} />
---
+++ src/components/welcome.js
@@ -0,0 +1,20 @@
+import '../styles/WelcomeStyle.css';
+
+export default function Welcome() {
+  return (
+    <div className="welcome">
+      <div className="welcome-container">
+        <div className="welcome-a">
+          <h1>Welcome to Destroyer</h1>
+        </div>
+        <div className="welcome-b">
+          A quick explanation of how this this works
+        </div>
+        <div className="welcome-c"></div>
+        <div className="welcome-d"></div>
+        <div className="welcome-e">Privacy Policy</div>
+        <div className="welcome-f"></div>
+      </div>
+    </div>
+  );
+}
