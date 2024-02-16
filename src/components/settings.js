4a090f1033f3c43d118a6f303ff6e148d59869df
mitchgruen <103967990+mitchgruen@users.noreply.github.com>
2024-02-16 14:56:14 -0500

    add personal links

--- src/components/settings.js
+++ src/components/settings.js
@@ -1,20 +1,42 @@
-import '../styles/SettingsStyle.css'
+import '../styles/SettingsStyle.css';
 
-function Settings({onClose, isSettingsOpen}) {
-
-  if (!isSettingsOpen) return null
+function Settings({ onClose, isSettingsOpen }) {
+  if (!isSettingsOpen) return null;
 
   return (
-    <div className='settings-container'>
-      <div className='settings-menu'>
-        <div className='close'>
+    <div className="settings-container">
+      <div className="settings-menu">
+        <div className="close">
           <button onClick={onClose}>
             <i className="fa-solid fa-x"></i>
           </button>
         </div>
-        <div className='header'/>
-        <div className='settings-body'>
-          <ul>
+        <div className="header" />
+        <div className="settings-body">
+          <div className="settings-content">
+            <h4>
+              Coming soon. In the meantime, check out one of my talks or articles:
+            </h4>
+            <ul>
+              <li>
+                <a href="https://drive.google.com/file/d/1FIoyouAGz42mMUAi2aYPclHFpP4Kmf2_/view">
+                  Recent talk on Event Streaming, Message Brokers, and Kafka
+                </a>
+              </li>
+              <li>
+                <a href="https://medium.com/@kafkometry/introducing-kafkometry-ae156ec6d205">
+                  Introducing Kafkometry, a Visualizer for Kafka Clusters
+                </a>
+              </li>
+              <li>
+                <a href="https://www.peoplenewspapers.com/2020/01/06/skilled-programmer-gets-lessons-in-entrepreneurship/">
+                  Skilled Programmer Gets Lessons in Entrepreneurship
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          {/* <ul>
             <li>Primary Color</li>
             <li>Random Color</li>
             <li>Night Mode</li>
@@ -23,11 +45,11 @@ function Settings({onClose, isSettingsOpen}) {
             <li>Keep Me Logged In</li>
             <li>Log Out</li>
             <li>Hide/Show Timers</li>
-          </ul>
+          </ul> */}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Settings;
--- src/components/trash.js
+++ src/components/trash.js
@@ -1,29 +1,45 @@
-import '../styles/TrashStyle.css'
+import '../styles/TrashStyle.css';
 
-function Trash({onClose, isTrashOpen}) {
-
-  if (!isTrashOpen) return null
+function Trash({ onClose, isTrashOpen }) {
+  if (!isTrashOpen) return null;
 
   return (
-    <div className='trash-container'>
-      <div className='trash-menu'>
-        <div className='close'>
+    <div className="trash-container">
+      <div className="trash-menu">
+        <div className="close">
           <button onClick={onClose}>
             <i className="fa-solid fa-x"></i>
           </button>
         </div>
-        <div className='header'/>
-        <div className='trash-body'>
-          <ul>
-            <li>Date Content Preview ...see more</li>
-            <li>Date Content Preview ...see more</li>
-            <li>Date Content Preview ...see more</li>
-            <li>Date Content Preview ...see more</li>
-          </ul>
+        <div className="header" />
+        <div className="trash-body">
+          <div className="trash-content">
+            <h4>
+              Coming soon. In the meantime, check out one of my talks or
+              articles:
+            </h4>
+            <ul>
+              <li>
+                <a href="https://drive.google.com/file/d/1FIoyouAGz42mMUAi2aYPclHFpP4Kmf2_/view">
+                  Recent talk on Event Streaming, Message Brokers, and Kafka
+                </a>
+              </li>
+              <li>
+                <a href="https://medium.com/@kafkometry/introducing-kafkometry-ae156ec6d205">
+                  Introducing Kafkometry, a Visualizer for Kafka Clusters
+                </a>
+              </li>
+              <li>
+                <a href="https://www.peoplenewspapers.com/2020/01/06/skilled-programmer-gets-lessons-in-entrepreneurship/">
+                  Skilled Programmer Gets Lessons in Entrepreneurship
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Trash;
