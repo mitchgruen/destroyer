047f47c22ac195430c19e7e88c1b967a0c22b9af
mitchgruen <103967990+mitchgruen@users.noreply.github.com>
2024-02-16 13:50:54 -0500

    delete more unused variables

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import store from './app/store';
 import { Provider } from 'react-redux';
 import Router from './components/router';
-import { ReactComponent as XIcon } from './assets/icons/xmark-sharp-regular.svg'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
