abc043cb701e45b3f487c2c996f15dcd3ed8e03d
mitchgruen <103967990+mitchgruen@users.noreply.github.com>
2024-04-11 10:55:35 -0400

    Remove / route in React Router, add index.html redirect to _redirects

--- src/components/router.js
+++ src/components/router.js
@@ -11,7 +11,6 @@ export default function Router() {
     <BrowserRouter>
       <Routes>
         <Route path="/app" element={<App />} />
-        <Route path="/" element={<Navigate replace to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/welcome" element={<Welcome />} />
