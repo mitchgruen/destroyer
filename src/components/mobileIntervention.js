adb3d187eaceb89406058cb3649dadaa3f01c4f2
mitchgruen <103967990+mitchgruen@users.noreply.github.com>
2024-04-24 11:20:03 -0400

    Add mobile intervention (err w/ prev push)

---
+++ src/components/mobileIntervention.js
@@ -0,0 +1,17 @@
+import '../styles/mobileInterventionStyle.css';
+
+function MobileIntervention() {
+  return (
+    <div className="container">
+      <div className="grid">
+        <div>Destroyer</div>
+        <div>Is</div>
+        <div>A</div>
+        <div>Desktop</div>
+        <div>App!</div>
+      </div>
+    </div>
+  );
+}
+
+export default MobileIntervention;
--- src/components/router.js
+++ src/components/router.js
@@ -4,20 +4,32 @@ import Register from './register';
 import ForgotPassword from './forgotpassword';
 import Welcome from './welcome';
 import Thanks from './thanks';
+import MobileIntervention from './mobileIntervention'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { isMobile } from 'react-device-detect';
 
 export default function Router() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/app" element={<App />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/welcome" element={<Welcome />} />
-        <Route path="/forgotpassword" element={<ForgotPassword />} />
-        <Route path="/thanks" element={<Thanks />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  const renderContent = () => {
+    if (isMobile) {
+      return (
+        <MobileIntervention/>
+      );
+    } else {
+      return (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/app" element={<App />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/welcome" element={<Welcome />} />
+            <Route path="/forgotpassword" element={<ForgotPassword />} />
+            <Route path="/thanks" element={<Thanks />} />
+          </Routes>
+        </BrowserRouter>
+      );
+    }
+  };
+
+  return renderContent();
 }
